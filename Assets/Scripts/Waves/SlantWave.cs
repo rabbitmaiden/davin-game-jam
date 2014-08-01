cbd6887bb4c147529fa218a554c37c6a96449656
Alice Rehfeldt <ojamarushealice@yahoo.com>
2014-07-31 17:02:09 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Resources/Prefabs/Enemies/SingleShotEnemy.prefab
    	Assets/Scenes/default.unity

--- Assets/Scripts/Waves/SlantWave.cs
+++ Assets/Scripts/Waves/SlantWave.cs
@@ -8,13 +8,13 @@ public class SlantWave : WaveScript {
 	public void Awake() {
 		enemyType = "SingleShotEnemy";
 		enemyPositions = new List<EnemyPosition>();
-		enemyPositions.Add(new EnemyPosition(-4.5f, 0f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(-3f, 0.7f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(-1.5f, 1.5f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(0f, 2.2f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition (1.5f, 2.9f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(3f, 3.6f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(4.5f, 4.3f, "SingleShotEnemy"));
+		enemyPositions.Add(new EnemyPosition(-4.5f, 0f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition(-3f, 0.7f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition(-1.5f, 1.5f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition(0f, 2.2f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition (1.5f, 2.9f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition(3f, 3.6f, "basic floater"));
+		enemyPositions.Add(new EnemyPosition(4.5f, 4.3f, "basic floater"));
 
 	}
 }
--- Assets/Scripts/Waves/WedgeWave.cs
+++ Assets/Scripts/Waves/WedgeWave.cs
@@ -8,12 +8,12 @@ public class WedgeWave : WaveScript {
 	public void Awake() {
 		enemyType = "SingleShotEnemy";
 		enemyPositions = new List<EnemyPosition>();
-		enemyPositions.Add(new EnemyPosition(0f, 2f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(1.5f, 5f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(-1.5f, 5f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(-3f, 3f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(3f, 3f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(4.5f, 0f, "SingleShotEnemy"));
-		enemyPositions.Add(new EnemyPosition(-4.5f, 0f, "SingleShotEnemy"));
+		enemyPositions.Add(new EnemyPosition(0f, 2f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(1.5f, 5f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(-1.5f, 5f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(-3f, 3f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(3f, 3f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(4.5f, 0f, "basic enemy"));
+		enemyPositions.Add(new EnemyPosition(-4.5f, 0f, "basic enemy"));
 	}
 }
